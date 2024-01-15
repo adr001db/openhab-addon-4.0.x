6081f8e4f67667cf80aa9109b85e253187573e48
Alexander Drent <81358972+adr001db@users.noreply.github.com>
2024-01-15 13:24:06 +0100

    Add Indoor unit

--- bundles/org.openhab.binding.daikin.onecta/src/main/java/org/openhab/binding/onecta/internal/OnectaBridgeHandlerFactory.java
+++ bundles/org.openhab.binding.daikin.onecta/src/main/java/org/openhab/binding/onecta/internal/OnectaBridgeHandlerFactory.java
@@ -22,10 +22,7 @@ import java.util.Set;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.onecta.internal.api.OnectaConnectionClient;
-import org.openhab.binding.onecta.internal.handler.OnectaBridgeHandler;
-import org.openhab.binding.onecta.internal.handler.OnectaDeviceHandler;
-import org.openhab.binding.onecta.internal.handler.OnectaGatewayHandler;
-import org.openhab.binding.onecta.internal.handler.OnectaWaterTankHandler;
+import org.openhab.binding.onecta.internal.handler.*;
 import org.openhab.binding.onecta.internal.service.DeviceDiscoveryService;
 import org.openhab.core.config.discovery.DiscoveryService;
 import org.openhab.core.i18n.TimeZoneProvider;
@@ -97,6 +94,8 @@ public class OnectaBridgeHandlerFactory extends BaseThingHandlerFactory {
             return new OnectaGatewayHandler(thing);
         } else if (thingTypeUID.equals((WATERTANK_THING_TYPE))) {
             return new OnectaWaterTankHandler(thing);
+        } else if (thingTypeUID.equals((INDOORUNIT_THING_TYPE))) {
+            return new OnectaIndoorUnitHandler(thing);
         }
         return null;
     }
